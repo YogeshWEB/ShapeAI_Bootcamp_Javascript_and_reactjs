d914b76bea85df14f2704849425ca6da72b5f97d
YogeshWEB <85472536+YogeshWEB@users.noreply.github.com>
2021-06-07 10:16:34 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>javascript and reactjs bootcamp</h1>
+      <p>This was the amazing bootcamp by ShapeAI</p>
+    </div>
+  );
+}
+export default Note;
